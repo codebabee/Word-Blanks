a781be6c279d495fbe31ebad210df60ef4af17ee
code_babee <>
2022-09-08 12:03:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My " + myAdjective + " brown " + myNoun + " " + myVerb + " to the park " + myAdverb;
+
+console.log(wordBlanks);
